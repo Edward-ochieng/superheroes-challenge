41e0b5143e150aa786e3afee6207a2c33b1f431f
Edward <edward.ochieng@student.moringaschool.com>
2022-12-01 19:30:40 +0300

    added code

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,36 @@
 class PowersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
+    def index
+        powers = Power.all
+        render json: powers
+    end
+
+    def show
+        power = find_power_id
+        render json: power
+    end
+
+    def update
+        power = find_power_id
+        power.update(power_params) 
+        render json: power
+    end
+
+
+
+
+    private
+
+
+    def power_params
+        params.permit(:name, :description)
+    end
+
+    def find_power_id
+        Power.find(params[:id])
+    end
+
+
+
 end
